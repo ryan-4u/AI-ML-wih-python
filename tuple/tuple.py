ffa9ecfe422a5f78022a0093f558d2e83a25e35a
Aaryan <aaryanofficial64@gmail.com>
2025-06-25 14:13:02 +0530

    tuple creation , acccess and updation and deletion

---
+++ tuple/tuple.py
@@ -0,0 +1,14 @@
+# tuples are unmutable liss
+#                     craeting tuples ( via () )
+tuple = () #empty tuple 
+print(tuple)
+tuple = (100,) # tuple with single element
+print(tuple)
+tuple = (100,56.78,True,"aaryan") #tuple with multiple element
+print(tuple)
+
+#                    accessimg tuple elments
+print(tuple[3]) # using index
+print(tuple[-2]) # using negative index
+print(tuple[1:2])
+
