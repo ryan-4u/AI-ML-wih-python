7d7c019d4e2dc0f6947d8defd8c3dcfe2f9813b4
Aaryan <aaryanofficial64@gmail.com>
2025-06-25 12:50:01 +0530

    creation updation and deletion in list

---
+++ lists/list.py
@@ -0,0 +1,19 @@
+                            # accessing list items
+list = ["apple",20,21.234,False]
+# print(list[0])
+
+# #                           updating list item
+# print ("Value available at index 2 : ")
+# print (list[2])
+# list[2] = 2001;
+# print ("New value available at index 2 : ")
+# print (list[2])
+
+#                             deleting list items
+list1 = ['physics', 'chemistry', 1997, 2000]
+# print (list1)
+# del list1[2];
+# print ("After deleting value at index 2 : ")
+# print (list1)
+list_X = list + list1
+print(list + list1)
