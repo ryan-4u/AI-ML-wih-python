9bb7d636064c8440e1c475240162167177d5e77e
Aaryan <aaryanofficial64@gmail.com>
2025-06-27 13:32:43 +0530

    some ques

---
+++ Conditional_loops/ques1.py
@@ -0,0 +1,12 @@
+marks = int(input("Enter your marks: "))
+if (marks>=90) :
+    print("A")
+elif (marks>=80) and (marks<90) :
+    print("B")
+elif marks>=70 and (marks<80) :
+    print("C")
+elif (marks>=60) and( marks<70) :
+    print("D")
+else:
+    print("F")    
+
